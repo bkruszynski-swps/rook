edd0979c635e41875121dbfb59f8a506c66ce1ca
bkruszynski@swps.edu.pl <bkruszynski@swps.edu.pl>
2020-08-02 20:09:48 +0000

    zmiany lokalne

---
+++ cluster/examples/kubernetes/yugabytedb/pslq-connect.sh
@@ -0,0 +1,7 @@
+#
+# 1. rookiem zanistaluj operatora
+# 2. zaistaluj kalster z bk-cluster.yaml - uzyj diffa aby zoabaczyc co nie trybi
+# 3. zainstaluj loadbalancera z loadbalancer*yaml
+# 4. i można się podłączać
+# 
+psql -h 192.168.22.27 -U yugabyte -p 5433
